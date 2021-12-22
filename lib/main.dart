d5a1b8c7232c8cd1a15864c41ec7c94e5a53511a
Julli Mayanne <julli.mayanne@gruposbf.com.br>
2021-12-22 20:52:04 -0300

    states

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,39 @@
 import 'package:flutter/material.dart';
+
 //stateless -> widgets que nao podem ser alterados
 //stateful -> widgets que podem ser alterados
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Series de natal",
+
     ///home: Container(color: Colors.white,),
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("Series"),
         backgroundColor: Colors.blue,
       ),
       body: Padding(
         padding: EdgeInsets.all(50),
-         child: Text("conteudo"),),
+        child: Text("conteudo"),
+      ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.green,
-        child:Padding(
-          padding: EdgeInsets.all(20),
-          child: Row(
-            children: [
-              Text("Home"),
-              Text("Post"),
-              Text("Email")
-            ],
-          )
-        ),
+        child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              children: [Text("Home"), Text("Post"), Text("Email")],
+            )),
       ),
-
-    ),
-  ));
+    );
+  }
 }
