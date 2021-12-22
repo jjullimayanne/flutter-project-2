c7b729cd25243c3886892bdce05358103e7ffd62
Julli Mayanne <julli.mayanne@gruposbf.com.br>
2021-12-22 20:46:13 -0300

    stateless

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+//stateless -> widgets que nao podem ser alterados
+//stateful -> widgets que podem ser alterados
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Series de natal",
+    ///home: Container(color: Colors.white,),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Series"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(50),
+         child: Text("conteudo"),),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.green,
+        child:Padding(
+          padding: EdgeInsets.all(20),
+          child: Row(
+            children: [
+              Text("Home"),
+              Text("Post"),
+              Text("Email")
+            ],
+          )
+        ),
+      ),
+
+    ),
+  ));
+}
