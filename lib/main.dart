b7b2456ec2aced1e34cb91368c41b49b470921a0
Julli Mayanne <julli.mayanne@gruposbf.com.br>
2021-12-27 13:33:16 -0300

    funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -17,48 +17,66 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var _frases = [
+    "Friends: Temporada 2: Episódio 9",
+    "Friends: Temporada 3: Episódio 10",
+    "Friends: Temporada 4: Episódio 10",
+    "Friends: Temporada 5: Episódio 10",
+    "Friends: Temporada 6: Episódio 10",
+    "Friends: Temporada 7: Episódio 10",
+    "Friends: Temporada 8: Episódio 11",
+    "Friends: Temporada 9: Episódio 10",
+    
+  ];
+  var _fraseGerada = "Clique abaixo para um ep especial de natal";
+  void _gerarFrase() {
+    var numeroSorteado = Random().nextInt(_frases.length);
+   
+    setState(() {
+       _fraseGerada = _frases [ numeroSorteado];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Episódios de natal"),
-        backgroundColor: Colors.red.shade900,
-      ),
-      body:Center(
-        child:  Container(
-       // width: double.infinity,
-        padding: EdgeInsets.all(16),
-        decoration:
-            BoxDecoration(border: Border.all(width: 3, color: Colors.black)),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset("images/image1.jpg"),
-            Text(
-              "Clique abaixo para um ep especial de natal",
-              textAlign: TextAlign.justify,
-              style: TextStyle(
-                  fontSize: 17,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.black),
-            ),
-            RaisedButton(
-              child: Text(
-                "New",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              color: Colors.red.shade900,
-              onPressed: () {},
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("Episódios de natal"),
+          backgroundColor: Colors.red.shade900,
         ),
-      ),
-      )
-    );
+        body: Center(
+          child: Container(
+            // width: double.infinity,
+            padding: EdgeInsets.all(16),
+            // decoration:
+            //  BoxDecoration(border: Border.all(width: 3, color: Colors.black)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset("images/image1.jpg"),
+                Text(
+                  _fraseGerada,
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                      fontSize: 17,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.black),
+                ),
+                RaisedButton(
+                    child: Text(
+                      "New",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    color: Colors.red.shade900,
+                    onPressed: _gerarFrase),
+              ],
+            ),
+          ),
+        ));
   }
 }
